8bdc43b4759feae61add5c9d8552b33ec026b031
Illiachr <74302872+Illiachr@users.noreply.github.com>
2020-12-04 15:59:29 +0200

    + remove items

--- script/app.js
+++ script/app.js
@@ -4,11 +4,10 @@ document.addEventListener('DOMContentLoaded', () =>
 
     const todoControl = document.querySelector('.todo-control'),
         headerInput = document.querySelector('.header-input'),
-        headerButton = document.querySelector('.header-button'),
         todoList = document.querySelector('.todo-list'),
         todoButtons = document.querySelectorAll('.todo-buttons'),
         todoRemove = document.querySelector('.todo-remove'),
-        todoComplete = document.querySelector('.todo-complete');
+        todoCompleted = document.querySelector('.todo-completed');
 
     const todoData = [
         {
@@ -20,9 +19,42 @@ document.addEventListener('DOMContentLoaded', () =>
             completed : true
         }
     ];
-
+    
     const render = () => {
-        console.log(todoData);
+        todoList.textContent = '';
+        todoCompleted.textContent = '';
+
+        todoData.forEach(item => {
+            const todoItem = document.createElement('li');
+            todoItem.classList.add('todo-item');
+            todoItem.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
+            '<div class="todo-buttons">' + 
+                '<button class="todo-remove"></button>' +
+                '<button class="todo-complete"></button>' +
+            '</div>';
+            if (item.completed) {
+                todoCompleted.append(todoItem);
+            } else {
+                todoList.append(todoItem);
+            }
+            const btnTodoComplete = todoItem.querySelector('.todo-complete');
+            btnTodoComplete.addEventListener('click', () => {
+                item.completed = !item.completed;
+                render();
+            });
+
+            const btnTodoRemove = todoItem.querySelector('.todo-remove');
+            btnTodoRemove.addEventListener('click', () => {                
+                let rmItem = todoData.findIndex(item => item.value === todoItem.textContent);
+                console.log(todoData[rmItem]);
+                todoData.splice(rmItem, 1);
+                render();
+            });
+        });
+
+        
+
+        //console.log(todoData);
         headerInput.value = '';
     };
 
